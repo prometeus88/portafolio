d1ba89d158c52dbb85c26c45065f31c43c40fdde
Benjamin Dominguez <bdominguez.avalos@gmail.com>
2018-02-21 17:41:03 -0600

    Menu creado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,10 @@ import {
 }  from "./components/index.paginas";
 
 const app_routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'item', component: ItemComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes, {useHash:true});
