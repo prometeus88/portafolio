d1b384ef09b64d21007c271bedd7f3067bdb7d2a
Benjamin Dominguez <bdominguez.avalos@gmail.com>
2018-02-26 17:40:06 -0600

    Se creo rutas para los productos.

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -14,9 +14,12 @@ export class ProductosService {
     this.cargando = true;
     this.http.get('https://paginaweb-6660c.firebaseio.com/productos_idx.json')
     .subscribe(res =>{
-    console.log(res.json());
-    this.cargando = false;
-    this.productos = res.json();
-  })
+    //console.log(res.json());
+    //setTimeout(()=>{
+      this.cargando = false;
+      this.productos = res.json();
+    //},1500)
+
+  });
 }
 }
